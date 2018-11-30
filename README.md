d30f555a44e7456e786fb731b1238c3386d923cc
Guillem <guillemchain@gmail.com>
2018-11-30 18:52:48 +0100

    CREATE repo and typescript base

---
+++ README.md
@@ -0,0 +1,1 @@
+# Human Biomechanics Domain Modeling
---
+++ test/health.spec.ts
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('Health test', () => {
+    it('success test', () => {
+        expect(true).to.be.equal(true);
+    });
+});
