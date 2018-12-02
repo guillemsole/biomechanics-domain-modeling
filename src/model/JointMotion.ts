a805ed7f1b8c13a27c0528263ebe85bdedd75095
Guillem <guillemchain@gmail.com>
2018-12-02 17:56:14 +0100

    ADDED JointMotion model and tests

---
+++ src/model/JointMotion.ts
@@ -0,0 +1,21 @@
+import {Ligament} from './Ligament';
+import {Movement} from './Movement';
+
+export class JointMotion {
+    // TODO: si té estat, ha de tenir id. Els value objects son stateless? Un value object pot contenir un value object
+    // TODO: Hi ha alguna caracterísitca de cadascun dels moviments que tingui sentit guardar
+    // TODO: Can we extend from this class
+    private _currentPosition: number;
+    constructor(public readonly ligament: Ligament,
+                currentPosition: number) {
+        this._currentPosition = currentPosition;
+    }
+
+    move(movement: Movement) {
+        if (!this.ligament.allowsMovementTo(movement.angle)) {
+            throw new Error('Cannot perform this movement');
+        }
+        this._currentPosition = movement.angle;
+        // FIRE domain event;
+    }
+}
