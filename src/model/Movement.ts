572f6dd8b8ee8609f74fd065bfd03acc5d2fc1b3
Guillem <guillemchain@gmail.com>
2018-12-02 17:55:24 +0100

    ADD movement tests

--- src/model/Movement.ts
+++ src/model/Movement.ts
@@ -1,3 +1,13 @@
-export class Movement {
+export enum MovementType { // TODO: or MotionType
+    FLEXIONEXTENSION,
+    ABDUCTIONADDUCTION,
+    ELEVATIONDEPRESSION,
+    ROTATION,
+}
 
+export class Movement {
+    constructor(public readonly type: MovementType,
+                public readonly angle: number) {
+
+    }
 }
--- test/model/Movement.spec.ts
+++ test/model/Movement.spec.ts
@@ -1,8 +1,25 @@
 import {expect} from 'chai';
-import {Movement} from '../../src/model/Movement';
+import {Movement, MovementType} from '../../src/model/Movement';
 
 describe('Movement', () => {
-    it('should create a movement', () => {
-        const movement: Movement = new Movement();
+    it('should create a FLEXIONEXTENSION movement', () => {
+        const movement: Movement = new Movement(MovementType.FLEXIONEXTENSION, 10);
+        expect(movement.type).to.be.equal(MovementType.FLEXIONEXTENSION);
+        expect(movement.angle).to.be.equal(10);
+    });
+    it('should create a ABDUCTIONADDUCTION movement', () => {
+        const movement: Movement = new Movement(MovementType.ABDUCTIONADDUCTION, 10);
+        expect(movement.type).to.be.equal(MovementType.ABDUCTIONADDUCTION);
+        expect(movement.angle).to.be.equal(10);
+    });
+    it('should create a ELEVATIONDEPRESSION movement', () => {
+        const movement: Movement = new Movement(MovementType.ELEVATIONDEPRESSION, 10);
+        expect(movement.type).to.be.equal(MovementType.ELEVATIONDEPRESSION);
+        expect(movement.angle).to.be.equal(10);
+    });
+    it('should create a ROTATION movement', () => {
+        const movement: Movement = new Movement(MovementType.ROTATION, 10);
+        expect(movement.type).to.be.equal(MovementType.ROTATION);
+        expect(movement.angle).to.be.equal(10);
     });
 });
