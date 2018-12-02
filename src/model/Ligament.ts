4ebd80e60ab518e858233076a70842c179d30285
Guillem <guillemchain@gmail.com>
2018-12-02 17:54:55 +0100

    ADDED Ligament tests

--- src/model/Ligament.ts
+++ src/model/Ligament.ts
@@ -1,8 +1,23 @@
 export class Ligament {
-    public readonly minExtension: number;
+    public readonly minExtension: number; // TODO: should be called muscle extension? or extension?
     public readonly maxExtension: number;
     constructor(minExtension: number, maxExtension: number) {
+        if (minExtension < 0) { // TODO: min must be always 0?
+            throw new Error('Min extension must be greater than 0');
+        }
+        if (maxExtension > 360) {
+            throw new Error('Max extension must be smaller than 360');
+        }
+
+        if (minExtension >= maxExtension) {
+            throw new Error('Max extension must be greater than min extension');
+        }
         this.minExtension = minExtension;
         this.maxExtension = maxExtension;
     }
+
+    allowsMovementTo(movementAngle: number): boolean { // TODO: change movement angle to model MovementAngle
+        return movementAngle >= this.minExtension && movementAngle <= this.maxExtension;
+    }
+
 }
--- test/model/Ligament.spec.ts
+++ test/model/Ligament.spec.ts
@@ -9,4 +9,46 @@ describe('Ligament test', () => {
         expect(ligament.minExtension).to.be.equal(minExtension);
         expect(ligament.maxExtension).to.be.equal(maxExtension);
     });
+
+    it('should throw error when minExtension is smaller than 0', () => {
+        expect(() => {
+            const ligament = new Ligament(-5, 10);
+        }).to.throw(Error);
+    });
+
+    it('should throw error when maxExtension is greater than 360', () => {
+        expect(() => {
+            const ligament = new Ligament(0, 400);
+        }).to.throw(Error);
+    });
+
+    it('should throw error when minExtension is equal to maxExtension', () => {
+        expect(() => {
+            const ligament = new Ligament(10, 10);
+        }).to.throw(Error);
+    });
+
+    it('should throw error when minExtension is greater than maxExtension', () => {
+        expect(() => {
+            const ligament = new Ligament(10, 0);
+        }).to.throw(Error);
+    });
+
+    it('should be in range', () => {
+        const ligament = new Ligament(10, 20);
+        expect(ligament.allowsMovementTo(10)).to.be.equal(true);
+        expect(ligament.allowsMovementTo(20)).to.be.equal(true);
+        expect(ligament.allowsMovementTo(15)).to.be.equal(true);
+
+    });
+
+    it('should be outside range', () => {
+        const ligament = new Ligament(10, 20);
+        expect(ligament.allowsMovementTo(5)).to.be.equal(false);
+    });
+
+    it('should throw', () => {
+        const ligament = new Ligament(10, 20);
+        expect(ligament.allowsMovementTo(25)).to.be.equal(false);
+    });
 });
